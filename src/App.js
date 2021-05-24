9f41f0e191a99d86103ad413e684d8171c1cc62f
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-05-24 08:35:22 +0700

    final init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { createContext, useState } from "react";
+import ClassContextComponent from "./ClassContextComponent";
+import FunctionContextComponent from "./FunctionContextComponent";
+
+export const ThemeContext = createContext();
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
+
   return (
-    <div className="App">
-      useContext
-    </div>
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <ClassContextComponent />
+        <FunctionContextComponent />
+      </ThemeContext.Provider>
+    </>
   );
 }
 
---
+++ src/FunctionContextComponent.js
@@ -0,0 +1,3 @@
+export default function FunctionContextComponent() {
+  return <div>Function Theme</div>;
+}
